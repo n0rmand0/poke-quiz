1807b020b20096dd4c509c320b247d7f8e2a8049
ricky.normandeau <rjnormandeau@gmail.com>
2022-06-03 10:30:47 -0400

    delete misc styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 /** @jsxImportSource @emotion/react */
 import { ReactComponent as Logo } from "./IndeedLogo.svg";
+import "./styles/global.scss";
 
 export default function App() {
   return (
-    <div css={{ padding: "0 16px" }}>
-    XX
+    <div >
+      <h1>Who's that Pokemon</h1>
     </div>
   );
 }
