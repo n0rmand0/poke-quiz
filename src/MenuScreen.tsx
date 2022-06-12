5b8d85779818f335dc34e4b0389fa3f79121f3fb
ricky.normandeau <rjnormandeau@gmail.com>
2022-06-11 21:52:24 -0400

    clean up

--- src/MenuScreen.tsx
+++ src/MenuScreen.tsx
@@ -11,12 +11,12 @@ export default function MenuScreen(props: any) {
       </a>
       <div className="instructions">
         <h2>Instructions:</h2>
-        <h4>
-          Guess the hidden Pokémon as quickly as you can.
-          <br /> You only have 5 seconds for each round.
-          <br /> The quicker you answer, the more points you will earn!
-          <br /> Every game is random, so keep trying!
-        </h4>
+        <p>
+          Guess the hidden Pokémon as quickly as you can. You only have 5
+          seconds for each round. The quicker you answer, the more points you
+          will earn!
+        </p>
+        <p>Every game is random, so keep trying!</p>
       </div>
     </div>
   );
